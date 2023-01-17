c9473d1514d600cb2c35febe697f9f07b6917c65
HoshinoHideki <dondake@yandex.ru>
2023-01-17 23:52:11 +0300

    Error handling

--- config/config.go
+++ config/config.go
@@ -12,24 +12,14 @@ import (
 	"runtime"
 )
 
-// config data container to be used in the main package.
 type Config struct {
 	Name string
 	OS   string
 }
 
-// Checks User's OS, then looks for the config file and tries to read the
-// data.
+// [Load]: Checks user's OS, then reads config data from file or creates new.
+func Load() (Config, error) {
 
-// If there is no config file, creates a default one.
-func Load() Config {
-
-	//question about configpath: Is it better to split it into path-filename,
-	//or it doesn't really matter?
-
-	//I initially wanted to split them, but got confused by string
-	//concatenation in Go and decided to opt into a single filepath+filename
-	//string for now.
 	var configPath string
 	switch runtime.GOOS {
 	case "windows":
@@ -38,30 +28,22 @@ func Load() Config {
 		log.Fatalf("unsupported OS: %s", runtime.GOOS)
 	}
 
-	conf := Config{} //TODO:I guess I can just delete this now?
-
-	// TODO: Maybe this should be the other way around now?
-	// Load existing conf by default, create one if not found/errored
-	// on loading.
-	if exists(configPath) {
-		fmt.Println("Found existing config file.")
-		// TODO: should I catch an error// make these funcs return possible
-		// errors too?
-		conf = loadExistingConfig(configPath)
-		// TODO: should check if everything is loaded successfully.
+	if !exists(configPath) {
+		fmt.Println("Config file not found. Creating a default one.")
+		conf, err := createDefaultConfig(configPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return conf, nil
 	}
-	fmt.Println("Config file not found. Creating a default one.")
 
-	conf = createDefaultConfig(configPath)
-
-	// minor thing: I don't really like "conf" variable name but I'm stumped
-	// how else to name it comprehensibly. Any ideas?
-	return conf
+	fmt.Println("Found existing config file.")
+	conf, err := loadExistingConfig(configPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return conf, nil
 }
-
-// Another question: is storing these functions outside Load() scope fine or
-// should have I put them inside Load()? I kinda get confused about
-// these things.
 
 // check file existence
 func exists(filepath string) bool {
@@ -74,13 +56,8 @@ func exists(filepath string) bool {
 	}
 }
 
-// Just to make sure: is it okay to abstract parts of code away into
-// separate fns like these? I like doing this because it makes it easier to
-// understand the overall flow, but I'm concerned if it unnecessarily
-// spagettifies the code?
-
 // Load config from file.
-func loadExistingConfig(filepath string) Config {
+func loadExistingConfig(filepath string) (Config, error) {
 	conf := Config{}
 
 	data, err := os.ReadFile(filepath)
@@ -92,14 +69,11 @@ func loadExistingConfig(filepath string) Config {
 	if err != nil {
 		log.Fatal(err)
 	}
-	return conf //err?
+	return conf, nil
 }
 
 // Create a new config, write to file and load it.
-func createDefaultConfig(filepath string) Config {
-
-	//Just making sure: Are these three comment lines fine?
-	//They kinda help to divide the code visually into functional bits.
+func createDefaultConfig(filepath string) (Config, error) {
 
 	//create the conf
 	currentUser, err := user.Current()
@@ -129,5 +103,5 @@ func createDefaultConfig(filepath string) Config {
 		log.Fatal(err)
 	}
 
-	return conf //err?
+	return conf, nil
 }
--- main.go
+++ main.go
@@ -8,7 +8,10 @@ import (
 func main() {
 	fmt.Println("it works!")
 	fmt.Println("Loading config.")
-	conf := config.Load()
+	conf, err := config.Load()
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Printf(
 		"Hello, %s. This line demonstrates that config "+
 			"was succesfully loaded.", conf.Name)
